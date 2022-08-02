fa502f352d89db6be4d5e802bbf901cd0b995fa0
jens <jens.steadman@gmail.com>
2022-08-02 16:14:35 -0600

    macro fun; plus dbt-utils macros

--- macros/get_payment_methods.sql
+++ macros/get_payment_methods.sql
@@ -1,13 +1,13 @@
-{% macro get_payment_methods() %}
+{% macro get_column_values(column_name, relation) %}
 
-    {% set payment_methods_query %}
+    {% set relation_query %}
         SELECT DISTINCT
-            payment_method
-        FROM jaffle_shop.raw_payments
-        ORDER BY payment_method
+            {{ column_name }}
+        FROM {{ relation }}
+        ORDER BY {{ column_name }}
     {% endset %}
 
-    {% set results = run_query(payment_methods_query) %}
+    {% set results = run_query(relation_query) %}
 
     {% if execute %}
     {# Return the first column #}
@@ -15,7 +15,16 @@
     {% else %}
         {% set results_list = [] %}
     {% endif %}
+    {{ log("**** before list check ****" + ": size, " + (results_list|length)|string, info=True) }}
+    {% for pm in results_list %}
+        {{ log(pm, info=True) }}
+    {% endfor %}
 
     {{ return(results_list) }}
 
+{% endmacro %}
+
+
+{% macro get_payment_methods() %}
+    {{ return(get_column_values('payment_method', ref('raw_payments'))) }}
 {% endmacro %}
