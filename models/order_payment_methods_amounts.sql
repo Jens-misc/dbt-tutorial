15d069f677fbbca0542196e51f5739e71f079b2e
jens <jens.steadman@gmail.com>
2022-08-02 11:15:29 -0600

    just testing git.

--- models/order_payment_methods_amounts.sql
+++ models/order_payment_methods_amounts.sql
@@ -2,8 +2,8 @@
 
 SELECT
     order_id,
-{%- for pm in payment_methods %}
-    IFNULL(SUM(CASE WHEN payment_method = '{{pm}}' THEN amount END), 0) AS {{pm}}_amount,
+{%- for pmo in payment_methods %}
+    IFNULL(SUM(CASE WHEN payment_method = '{{pmo}}' THEN amount END), 0) AS {{pm}}_amount,
 {%- endfor %}
     IFNULL(SUM(amount), 0) AS total_amount
 FROM {{ ref('raw_payments') }}
